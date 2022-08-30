d20cb5a67919c0e554a0791cfb0ed7f68a1bbc0b
bytefucker <yihongzhi@cestc.cn>
2022-08-30 17:36:26 +0800

    fix mapper重复方法

--- base-starter/base-mybatis-starter/src/main/java/com/moyu/framework/mybatis/mapper/BaseMapper.java
+++ base-starter/base-mybatis-starter/src/main/java/com/moyu/framework/mybatis/mapper/BaseMapper.java
@@ -2,7 +2,6 @@ package com.moyu.framework.mybatis.mapper;
 
 import com.moyu.framework.core.entity.Entity;
 import io.mybatis.mapper.Mapper;
-import io.mybatis.mapper.fn.FnMapper;
 import io.mybatis.mapper.list.ListMapper;
 import java.io.Serializable;
 
@@ -13,6 +12,6 @@ import java.io.Serializable;
  * @date 2022/8/19
  */
 public interface BaseMapper<T extends Entity<PK>, PK extends Serializable>
-    extends Mapper<T, PK>, ListMapper<T>, FnMapper<T> {
+    extends Mapper<T, PK>, ListMapper<T> {
 
 }
